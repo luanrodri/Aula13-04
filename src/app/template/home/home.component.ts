0934f44d0b2a8ae1f7455dcaca1ec393f0b6e732
Etec Professor Horácio Augusto da Silveira <luanrds_2000@hotmail.com>
2023-04-13 22:15:00 -0300

    Atualização do Frontend, Utilização do TypeScript

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -6,12 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  public nomeProduto: string = "Curso Angular";
+  anuncio: string = `O  ${this.nomeProduto} está em promoção`;
+  idProduto: number = 123;
+  precoProduto: number = 2.59;
+  promocao: boolean = true;
 
   constructor() {
- //   this.nomeFunc='João'
+   // this.anuncio = 'O' + this.nomeProduto + 'está em promoção';
+   console.log('Nome do Produto', this.nomeProduto);
+   console.log('Anuncio', this.anuncio);
+   console.log('Id', this.precoProduto);
+   console.log('Promoção', this.promocao);
 
   }
-
-
 }
