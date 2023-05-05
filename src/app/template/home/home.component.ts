5f59934c86cdd132918870c1164fd62dacb8f9f1
Luan <luanrds_2000@hotmail.com>
2023-05-04 22:36:10 -0300

    Aula para criar pipes e Outras coisas, Corrigir erros

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -1,23 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public nomeProduto: string = "Curso Angular";
   anuncio: string = `O  ${this.nomeProduto} está em promoção`;
-  idProduto: number = 123;
+  <p class = "card-text"> Identificação: {{idProduto| number = '2.0-3'}} </p>
   precoProduto: number = 2.59;
   promocao: boolean = true;
+  foto:string = 'assets/img/crud.png'
 
   constructor() {
    // this.anuncio = 'O' + this.nomeProduto + 'está em promoção';
-   console.log('Nome do Produto', this.nomeProduto);
-   console.log('Anuncio', this.anuncio);
-   console.log('Id', this.precoProduto);
-   console.log('Promoção', this.promocao);
-
+console.log("Nome do Produto", this.nomeProduto);
   }
+  ngOnInit(): void{}
 }
