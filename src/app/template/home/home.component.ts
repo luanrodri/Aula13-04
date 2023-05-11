b5bf54df16bf5a639da7be3e1f202452ed718e64
Luan <luanrds_200@hotmail.com>
2023-05-11 20:51:05 -0300

    Atualização do código, criação de Pipes, código arrumado

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -5,17 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   public nomeProduto: string = "Curso Angular";
   anuncio: string = `O  ${this.nomeProduto} está em promoção`;
-  <p class = "card-text"> Identificação: {{idProduto| number = '2.0-3'}} </p>
-  precoProduto: number = 2.59;
+  idProduto: number = 3;
+  precoProduto: number = 2.51;
   promocao: boolean = true;
-  foto:string = 'assets/img/crud.png'
+  foto: string = 'assets/img/crud.png';
+  dataValidade = '2021-12-31';
+
 
   constructor() {
    // this.anuncio = 'O' + this.nomeProduto + 'está em promoção';
 console.log("Nome do Produto", this.nomeProduto);
   }
-  ngOnInit(): void{}
+
 }
