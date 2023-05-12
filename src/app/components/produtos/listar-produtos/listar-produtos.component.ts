717633cb52d4d4d0dfa12c3b5afca939e771364d
Luan <luanrds_200@hotmail.com>
2023-05-11 22:29:09 -0300

    Aula 11.05,2023, nesta aula usamos Arrays, Objetos, For e ngFor, agora nossa aplicação há os tres cursos e seus preços convertidos para BRL.

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-produtos.component.css']
 })
 export class ListarProdutosComponent {
+  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumeros: number[] = [15, 15.18, 100];
+
+  objetoModelo = {
+    nome: 'Luan',
+    idade: 22,
+    altura: 1.83,
+    graduado: true
+  };
+
+  listaProdutos: any[] = [
+    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2021-12-31', id:1},
+    {nome: 'Curso de Ionic', precoProduto:50, validade:'2021-12-31', id:2, promocao: true},
+    {id: 3, nome: 'Curso de Ionic Avançado', precoProduto: 50, validade: '2021-12-31'},
+  ];
+
+
+  constructor(){
+    for (let item of this.listaStrings){
+      console.log(item);
+    }
+    for(const item of this.listaNumeros){
+      console.log(item);
+    }
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
+  }
 
 }
